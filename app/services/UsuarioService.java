a411a14129d0b7b96b275950eec4c3bb6611b308
Quico Llinares <qll1@alu.ua.es>
2017-09-18 11:50:55 +0200

    Se impide crear usuarios con logins ya existentes

--- app/services/UsuarioService.java
+++ app/services/UsuarioService.java
@@ -17,8 +17,14 @@ public class UsuarioService {
   }
 
   public Usuario creaUsuario(String login, String email, String password) {
+    if (repository.findByLogin(login) != null) {
+      throw new UsuarioServiceException("Login ya existente");
+    }
      Usuario usuario = new Usuario(login, email);
      usuario.setPassword(password);
      return repository.add(usuario);
+  }
+  public Usuario findUsuarioPorLogin(String login) {
+     return repository.findByLogin(login);
   }
 }
