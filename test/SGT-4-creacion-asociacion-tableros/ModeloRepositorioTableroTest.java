f504179ea3ca1e4f13aecb6074dca8c62ff533b8
Quico Llinares <qll1@alu.ua.es>
2017-10-13 18:33:42 +0200

    Añadido método add() en TableroRepository

--- test/SGT-4-creacion-asociacion-tableros/ModeloRepositorioTableroTest.java
+++ test/SGT-4-creacion-asociacion-tableros/ModeloRepositorioTableroTest.java
@@ -20,9 +20,11 @@ import java.sql.*;
 import models.Usuario;
 import models.Tablero;
 import models.TableroRepository;
+import models.UsuarioRepository;
 
 public class ModeloRepositorioTableroTest {
   static private Injector injector;
+  static Database db;
 
   @BeforeClass
   static public void initApplication() {
@@ -31,6 +33,7 @@ public class ModeloRepositorioTableroTest {
     injector = guiceApplicationBuilder.injector();
     // Necesario para inicializar JPA
     injector.instanceOf(JPAApi.class);
+    db = injector.instanceOf(Database.class);
   }
 
   //Test #32
@@ -50,13 +53,39 @@ public class ModeloRepositorioTableroTest {
     TableroRepository tableroRepository = injector.instanceOf(TableroRepository.class);
     assertNotNull(tableroRepository);
   }
+
   //Test #34
   @Test
   public void testCrearTablaTableroEnBD() throws Exception {
-    Database db = injector.instanceOf(Database.class);
     Connection connection = db.getConnection();
     DatabaseMetaData meta = connection.getMetaData();
     ResultSet res = meta.getTables(null, null, "TABLERO", new String[] {"TABLE"});
     assertTrue(res.next());
+  }
+
+  //Test #35
+  @Test
+  public void testAddTableroInsertsDatabase() {
+    UsuarioRepository usuarioRepository = injector.instanceOf(UsuarioRepository.class);
+    TableroRepository tableroRepository = injector.instanceOf(TableroRepository.class);
+    Usuario administrador = new Usuario("juangutierrez", "juangutierrez@gmail.com");
+    administrador = usuarioRepository.add(administrador);
+    Tablero tablero = new Tablero(administrador, "Tablero 1");
+    tablero = tableroRepository.add(tablero);
+    assertNotNull(tablero.getId());
+    assertEquals("Tablero 1", getNombreFromTableroDB(tablero.getId()));
+  }
+
+  //Test #36
+  private String getNombreFromTableroDB(Long tableroId) {
+    String nombre = db.withConnection(connection -> {
+       String selectStatement = "SELECT Nombre FROM Tablero WHERE ID = ? ";
+       PreparedStatement prepStmt = connection.prepareStatement(selectStatement);
+       prepStmt.setLong(1, tableroId);
+       ResultSet rs = prepStmt.executeQuery();
+       rs.next();
+       return rs.getString("Nombre");
+    });
+    return nombre;
   }
 }
