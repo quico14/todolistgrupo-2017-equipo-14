6ff427409852bdc7ad9f1fb13b5dbab72c6e95e6
Quico Llinares <qll1@alu.ua.es>
2017-12-14 18:29:15 +0100

    Añadido SizeService, actualizando tests

---
+++ app/services/SizeServiceException.java
@@ -0,0 +1,10 @@
+package services;
+
+public class SizeServiceException extends RuntimeException {
+
+  public SizeServiceException() {}
+
+  public SizeServiceException(String message) {
+     super(message);
+  }
+}
